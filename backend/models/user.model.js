5e94a088313eaca9b349ccb922c2671bacda973d
achla dhiman <achladhiman3@gmail.com>
2024-12-28 12:11:07 +1300

    user model remove fullname field

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -4,17 +4,7 @@ import bcrypt from "bcrypt";
 
 
 const userSchema = new mongoose.Schema({
-    fullname: {
-        firstname: {
-            type: String,
-            required: true,
-            minlength: [ 3, 'First name must be at least 3 characters long' ],
-        },
-        lastname: {
-            type: String,
-            minlength: [ 3, 'Last name must be at least 3 characters long' ],
-        }
-    },
+    
     email: {
         type: String,
         required: true,
@@ -25,10 +15,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         select: false,
-    },
-    socketId: {
-        type: String,
-    },
+    }, 
 })
 
 userSchema.methods.generateAuthToken = function () {
