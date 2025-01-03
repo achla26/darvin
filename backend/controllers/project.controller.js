e803374d0e40dbe768ddd0c4ec5df24117f89c08
achla dhiman <achlar8@gmail.com>
2025-01-03 20:26:20 +1300

    projects end point created

--- backend/controllers/project.controller.js
+++ backend/controllers/project.controller.js
@@ -1,7 +1,7 @@
 import { validationResult } from "express-validator";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
-import { addProject } from "../services/project.service.js";
+import { addProject , getProjectsByUserId , updateProjectById , addNewUserToProject}  from "../services/project.service.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 export const createProject = asyncHandler(async (req, res, next) => {
@@ -25,5 +25,67 @@ export const createProject = asyncHandler(async (req, res, next) => {
     // Send response
     return res.status(201).json(
         new ApiResponse(201, { project }, "Project created successfully.")
+    );
+});
+
+export const getProjects = asyncHandler(async (req, res, next) => {
+    const user_id = req.user?._id;
+
+    if(!user_id){
+        throw new ApiError(401, "Unauthorized: User ID not found.");
+    }
+
+    const projects = await getProjectsByUserId(user_id);
+
+    return res.status(200).json(
+        new ApiResponse(200, { projects }, "Projects retrieved successfully.")
+    );
+
+});
+
+export const updateProject = asyncHandler(async (req, res, next) => {   
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name } = req.body;
+    const { id } = req.params;
+    const userId = req.user?._id;
+
+    if (!userId) {
+        throw new ApiError(401, "Unauthorized: User ID not found.");
+    }
+
+    // Update the project using the service
+    const project = await updateProjectById(id, name, userId);
+
+    // Send response
+
+    return res.status(200).json(
+        new ApiResponse(200, { project }, "Project updated successfully.")
+    );
+    
+});
+
+export const addUserToProject = asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { projectId, users } = req.body;
+    const userId = req.user?._id;
+
+    if (!userId) {
+        throw new ApiError(401, "Unauthorized: User ID not found.");
+    }
+
+    // Add the user to the project using the service
+    const project = await addNewUserToProject(projectId, users, userId);
+
+    // Send response
+    return res.status(200).json(
+        new ApiResponse(200, { project }, "User added to project successfully.")
     );
 });
--- backend/routes/project.router.js
+++ backend/routes/project.router.js
@@ -1,17 +1,37 @@
 import { Router } from "express";
 import { body } from "express-validator";
-import { createProject } from "../controllers/project.controller.js"; 
+import { createProject , getProjects , updateProject , addUserToProject} from "../controllers/project.controller.js"; 
 import { authUser } from "../middlewares/auth.middleware.js";
 
 
 const router = Router();
 
-
 router.post('/create', [
-    body("name") 
-    .isString().withMessage("Name must be a string") 
-],authUser,createProject
-) 
+        body("name") 
+        .isString().withMessage("Name must be a string") 
+    ],
+    authUser,createProject
+);
+
+router.get('/all', authUser,getProjects);
+
+router.put('/update/:id', 
+    [   
+        body("name") 
+        .isString().withMessage("Name must be a string")
+    ],
+    authUser,updateProject
+);
 
 
-export default router
+router.put('/add-user',
+    authUser,
+    [
+        body('projectId').isString().withMessage('Project ID is required'),
+        body('users').isArray({ min: 1 }).withMessage('Users must be an array of strings').bail()
+        .custom((users) => users.every(user => typeof user === 'string')).withMessage('Each user must be a string')
+    ],
+    addUserToProject
+)
+
+export default router;
